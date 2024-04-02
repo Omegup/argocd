3380d473e8380c231f80c6c149e395f6d2789ae5
Mohamed Ali CHARRAD <charradmedali@gmail.com>
2024-04-02 09:08:33 +0000

    Commit message

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
+git config user.name "Mohamed Ali CHARRAD"
+git config user.email "charradmedali@gmail.com"
 git add .
-
-git commit -m "argocd commit"
-
+git commit -m "Commit message"
 git push
+
