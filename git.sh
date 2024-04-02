0ca58d83fd67bbd50c75557d785df7d70cba9d00
Mohamed Ali CHARRAD <charradmedali@gmail.com>
2024-04-02 09:14:31 +0000

    Commit message

--- git.sh
+++ git.sh
@@ -2,6 +2,8 @@
 
 git config user.name "Mohamed Ali CHARRAD"
 git config user.email "charradmedali@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add /home/omegup/.ssh/id_rsa
 git add .
 git commit -m "Commit message"
 git push
